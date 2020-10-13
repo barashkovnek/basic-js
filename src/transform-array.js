9ca0fd68c6bd575dc06bbcb9f71e0ea4f9e40b0c
barashnek <barashnek@gmail.com>
2020-10-13 11:35:41 +0600

    docs: change files

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) { throw new Error(); }
+  // if (arr.length === 0) { return arr; }
+
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "--discard-prev") {
+      newArr.pop();
+
+    } else if (arr[i] === "--double-next") {
+      if (arr[i + 1] !== undefined) {
+        newArr.push(arr[i + 1]);
+      }
+
+    } else if (arr[i] === "--double-prev") {
+      if (arr[i - 1] !== undefined) {
+        newArr.push(arr[i - 1]);
+      }
+
+    } else if (arr[i] === "--discard-next") {
+      if (arr[i + 2] && arr[i + 2].toString().includes('-prev')) {
+        i = i + 2;
+      }
+      else {
+        i = i + 1;
+      }
+
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 };
+
+
+
+
+
+
+
+
+
--- src/what-season.js
+++ src/what-season.js
@@ -1,7 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  if (date instanceof Date || !isNaN(date)) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  if (Object.prototype.toString.call(date) === '[object Date]') {
     if (date.getMonth() === 11 || date.getMonth() === 0 || date.getMonth() === 1) {
       return 'winter';
     } else if (date.getMonth() === 2 || date.getMonth() === 3 || date.getMonth() === 4) {
@@ -11,9 +14,8 @@ module.exports = function getSeason(date) {
     } else if (date.getMonth() === 8 || date.getMonth() === 9 || date.getMonth() === 10) {
       return 'autumn';
     }
-  } else if (date !== undefined) {
-    throw new CustomError('THROWN');
   } else {
-    return 'Unable to determine the time of year!';
+    throw new Error();
   }
 };
+
